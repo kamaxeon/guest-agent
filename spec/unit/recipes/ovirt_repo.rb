d733275a289b8071f8b30c89cf94a13d0bf18b6f
Israel Santana <isra@miscorreos.org>
2015-05-23 00:51:17 +0100

    Fix spec ovirt_repo test

--- spec/unit/recipes/ovirt_repo.rb
+++ spec/unit/recipes/ovirt_repo.rb
@@ -1,8 +1,11 @@
 require 'chefspec'
+require 'chefspec/berkshelf'
 
 describe 'guest-agent::ovirt_repo' do
   let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
+  # let(:chef_run) { ChefSpec::SoloRunner.new('..').converge(described_recipe) }
 
+  ChefSpec::SoloRunner.new(cookbook_path: '/home/isra/.berkshelf/cookbooks/')
   
   it 'install ovirt-guest-agent package' do
     expect(chef_run).to install_package('ovirt-guest-agent')
