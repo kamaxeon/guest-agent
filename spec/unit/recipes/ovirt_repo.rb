5fe956b074fc1e838d6fab08e2e812b4a99000fd
Israel Santana <isra@miscorreos.org>
2015-05-23 12:32:09 +0100

    Test for Ubuntu 12.04, 14.04 and CentOS 6.4 and 7.0

--- spec/unit/recipes/ovirt_repo.rb
+++ spec/unit/recipes/ovirt_repo.rb
@@ -1,5 +1,5 @@
 require 'chefspec'
-require 'chefspec/berkshelf'
+#require 'chefspec/berkshelf'
 
 describe 'guest-agent::ovirt_repo' do
   let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
@@ -14,37 +14,42 @@ end
 
 # Regression Tests
 
-describe 'CentOS setup' do
-  let(:chef_run) do
-    runner = ChefSpec::SoloRunner.new(
-      platform: 'centos',
-      version:  '6.3'
-      )
-    runner.converge('guest-agent::ovirt_repo')
-  end
+%w(6.4 7.0).each do |version| 
+  describe "CentOS #{version} setup" do
+    let(:chef_run) do
+      runner = ChefSpec::SoloRunner.new(
+        platform: 'centos',
+        version:  version
+        )
+      runner.converge('guest-agent::ovirt_repo')
+    end
 
-  it 'should include the yum recipe' do
-    expect(chef_run).to include_recipe 'yum'
-  end
+    it 'should include the yum recipe' do
+      expect(chef_run).to include_recipe 'yum'
+    end
 
-  it 'should include the yum epel recipe' do
-    expect(chef_run).to include_recipe 'yum-epel'
-  end
+    it 'should include the yum epel recipe' do
+      expect(chef_run).to include_recipe 'yum-epel'
+    end
 
-  it 'should include the selinux::disable recipe' do
-    expect(chef_run).to include_recipe 'selinux::disabled'
+    it 'should include the selinux::disable recipe' do
+      expect(chef_run).to include_recipe 'selinux::disabled'
+    end
   end
 end
 
-describe 'Ubuntu setup' do
-  let(:chef_run) do
-    runner = ChefSpec::SoloRunner.new(
-      platform: 'ubuntu',
-      version:  '12.04'
-      )
-    runner.converge('guest-agent::ovirt_repo')
-  end
-  it 'should include the apt recipe' do
-    expect(chef_run).to include_recipe 'apt::default'
+
+%w(12.04 14.04).each do |version|
+  describe "Ubuntu #{version} setup" do
+    let(:chef_run) do
+      runner = ChefSpec::SoloRunner.new(
+        platform: 'ubuntu',
+        version:  version
+        )
+      runner.converge('guest-agent::ovirt_repo')
+    end
+    it 'should include the apt recipe' do
+      expect(chef_run).to include_recipe 'apt::default'
+    end
   end
 end
